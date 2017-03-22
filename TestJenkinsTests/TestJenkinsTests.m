ceb68bc1c4cfe0c49f5570d7fb0d95bd6ad1f024
jack wong <jackwong@jackdeMacBook-Pro.local>
2017-03-22 19:24:20 +0800

    add unittest

--- TestJenkinsTests/TestJenkinsTests.m
+++ TestJenkinsTests/TestJenkinsTests.m
@@ -24,6 +24,10 @@
     [super tearDown];
 }
 
+- (void)testDemo {
+    XCTAssertEqual(1, 2);
+}
+
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
