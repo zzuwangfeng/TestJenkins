1f109acbf8d7b2464dd7d0e7ce644dbb0ba4153a
zzuwangfeng <zzuwangfeng@163.com>
2017-03-24 13:04:03 +0800

    add test1

---
+++ TestJenkins/test2.m
@@ -0,0 +1,21 @@
+//
+//  test2.m
+//  TestJenkins
+//
+//  Created by  on 2017/3/24.
+//  Copyright © 2017年 jack wong. All rights reserved.
+//
+
+#import "test2.h"
+
+@implementation test2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
