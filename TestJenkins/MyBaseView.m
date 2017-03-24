82e226287f7ef9614d6aaf7590fc56d592350ae7
zzuwangfeng <zzuwangfeng@163.com>
2017-03-24 11:31:15 +0800

    add mybaseview

---
+++ TestJenkins/MyBaseView.m
@@ -0,0 +1,21 @@
+//
+//  MyBaseView.m
+//  TestJenkins
+//
+//  Created by  on 2017/3/24.
+//  Copyright © 2017年 jack wong. All rights reserved.
+//
+
+#import "MyBaseView.h"
+
+@implementation MyBaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
