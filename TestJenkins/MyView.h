3b385a133c13822ed6f510bcc5f13e3eb3352e6b
zzuwangfeng <zzuwangfeng@163.com>
2017-03-24 11:22:48 +0800

    update

---
+++ TestJenkins/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  TestJenkins
+//
+//  Created by  on 2017/3/24.
+//  Copyright © 2017年 jack wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
---
+++ TestJenkins/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  TestJenkins
+//
+//  Created by  on 2017/3/24.
+//  Copyright © 2017年 jack wong. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
