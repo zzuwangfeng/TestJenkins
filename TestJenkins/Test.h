4e6491567ceabd8075ff02cb935fe60f7af7a461
zzuwangfeng <zzuwangfeng@163.com>
2017-03-24 11:04:29 +0800

    add testdemofunction

---
+++ TestJenkins/Test.h
@@ -0,0 +1,16 @@
+//
+//  Test.h
+//  TestJenkins
+//
+//  Created by  on 2017/3/24.
+//  Copyright © 2017年 jack wong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+
+
+
+@end
---
+++ TestJenkins/Test.m
@@ -0,0 +1,15 @@
+//
+//  Test.m
+//  TestJenkins
+//
+//  Created by  on 2017/3/24.
+//  Copyright © 2017年 jack wong. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+-        (void)testDemo {
+    
+}
+@end
